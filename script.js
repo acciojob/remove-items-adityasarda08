c185db0e6aba2e369389757fee384fa1e902b852
adityasarda08 <142217693+adityasarda08@users.noreply.github.com>
2026-01-29 22:09:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const rmitem = document.getElementById("colorSelect");
+const button = document.querySelector('input[type="button"]');
+
+button.addEventListener("click" ,()=>{
+ const selectedIndex = rmitem.selectedIndex
+
+	if(selectedIndex !== -1){
+		rmitem.remove(selectedIndex);
+	}
+})
+	
+
